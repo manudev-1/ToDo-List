8735a64087851f244aef860d61dc9a69cbae5a85
manue <manue@DESKTOP-C47717A>
2023-02-02 17:19:40 +0100

    SetUp and Start

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'opac': 'rgba(0, 0, 0, 0.5)'
+      }
+    },
   },
   plugins: [],
 }
