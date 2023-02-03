bd99cfed726856388ed2fec2f69247bbc46279bf
manue <manue@DESKTOP-C47717A>
2023-02-03 17:32:51 +0100

    BG Text Start

--- src/CookieAccepts.js
+++ src/CookieAccepts.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 function CookieAccepts() {
 
@@ -6,13 +7,15 @@ function CookieAccepts() {
 
   p.forEach((element) => {
     var char = element.innerHTML.split("")
+    
   })
+  const [randLeft, setRandLeft] = useState(Math.floor(Math.random() * document.body.clientWidth))
 
   return (
     <div className="CookieAccepts">
       <div className="w-screen h-screen bg-gradient-to-tl from-violet-400 via-indigo-600 to-fuchsia-500 flex justify-center items-center">
-        <p className='text-6xl absolute font-bold text-opac z-0' id='p'>TODO LIST</p>
-        <p className='text-6xl absolute font-bold text-opac z-0' id='p'>TODO LIST</p>
+        <p className='text-6xl absolute font-bold text-opac z-0 duration-300' id='p' style={{'left': randLeft}}>TODO LIST</p>
+        <p className='text-6xl absolute font-bold text-opac z-0 duration-300' id='p' style={{'left': randLeft}}>TODO LIST</p>
         <div className="w-72 h-72 bg-white rounded-lg flex flex-col items-center z-10">
           <h1 className='m-4 font-medium text-2xl'>ToDo List</h1>
           <p className='mx-4 text-center font-extralight'>This site needs your consent to storage the information.</p>
