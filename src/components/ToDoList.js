0cd7252efe6edbfc08969d3525e154cf98f62d35
manue <manue@DESKTOP-C47717A>
2023-02-06 21:40:19 +0100

    Draggable and Stuff's Style Edit

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "../App.css";
 import trashCan from "../assets/trash-can.svg";
@@ -6,29 +6,37 @@ import drag from "../assets/drag.svg";
 
 function TODO_LIST() {
   // ! Input Edit
+  // * Focus Var
   const [inputFocus, setInputFocus] = useState(false);
 
+  // * Focus Detection
   const handleInputFocus = () => {
     setInputFocus(true);
   };
 
+  // * Blur Detection
   const handleInputBlur = () => {
     setInputFocus(false);
   };
 
   // ! Title
+  // * Title Var
   const [title, setTitle] = useState("");
 
+  // * Title Modify
   const handleTitle = () => {
     document.title = "ToDo List |";
     setTitle(document.getElementById("txAdd").value);
     document.title += " " + title.charAt(0).toUpperCase() + title.slice(1);
   };
 
-  // ! Todolist
+  // ! ToDolist
+  // * ToDoList Var
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [isDragged, setDragged] = useState(false);
 
+  // * Input from btn
   const handleInputData = () => {
     if (input !== "") {
       const id = todoList.length + 1;
@@ -39,13 +47,14 @@ function TODO_LIST() {
           task: input,
           complete: false,
           trashNear: false,
-          delete: false
+          deleted: false,
         },
       ]);
       setInput("");
     }
   };
 
+  // * Input from KB
   const handleInputDataKB = (e) => {
     if (e.key === "Enter")
       if (input !== "") {
@@ -57,13 +66,14 @@ function TODO_LIST() {
             task: input,
             complete: false,
             trashNear: false,
-            deleted: false
+            deleted: false,
           },
         ]);
         setInput("");
       }
   };
 
+  // * Complete Task
   const handleCompleteTask = (id) => {
     let list = todoList.map((task) => {
       let item = {};
@@ -75,36 +85,44 @@ function TODO_LIST() {
     setTodoList(list);
   };
 
+  // * Manage Near to TrashCan Icon
   const handleDistance = (id) => {
     let list = todoList.map((task) => {
       let item = {};
-      if (task.id === id)
-        item = { ...task, trashNear: !task.trashNear };
+      if (task.id === id) item = { ...task, trashNear: !task.trashNear };
       else item = { ...task };
       return item;
     });
     setTodoList(list);
   };
 
+  // * Delete Task
   const handleDelete = (id) => {
     let list = todoList.map((task) => {
-      let item = {}
-      if (task.id === id)
-        item = { ...task, deleted: true }
-      else item = { ...task }
-      return item
+      let item = {};
+      if (task.id === id) item = { ...task, deleted: true };
+      else item = { ...task };
+      return item;
     });
-    setTodoList(list)
+    setTodoList(list);
   };
 
+  // * Drag Memory Function
   const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
     const list = Array.from(todoList);
     const [reorderedTask] = list.splice(result.source.index, 1);
     list.splice(result.destination.index, 0, reorderedTask);
 
-    setTodoList(list)
-  }
-  
+    setDragged(false);
+    setTodoList(list);
+  };
+
+  // * Dragged or Not
+  const handleDrag = () => {
+    if(todoList.length > 1)
+      setDragged(true);
+  };
 
   return (
     <div className="ToDo_List font-lato">
@@ -121,7 +139,7 @@ function TODO_LIST() {
                 : "outline-none border-none h-10 w-5/6 rounded-lg p-2 duration-500 capitalize placeholder:text-gray-200 bg-black text-gray-200"
             }
             placeholder="What will you do?"
-            maxLength={15}
+            maxLength={35}
             id="txAdd"
             value={input}
             onFocus={handleInputFocus}
@@ -141,66 +159,89 @@ function TODO_LIST() {
             Add
           </button>
         </div>
-        <div className="w-1/6 m-5 text-gray-600">
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-              <Droppable droppableId="taks">
-                {(provided) => (
-                  <section {...provided.droppableProps} ref={provided.innerRef}>
-                    {todoList.map((task, index) => {
-                      return (
-                        <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
-                          {(provided) => (
-                            <section className="flex items-center" {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
-                              <div
-                                className={task.deleted ? 'hidden' :"flex justify-between items-center border-2 border-solid border-black border-collapse px-5 py-2 min-w-full my-2"}
-                                complete={String(task.complete)}
-                                onClick={() => handleCompleteTask(task.id)}
-                              >
-                                <p
-                                  className={
-                                    task.complete
-                                      ? "capitalize text-gray-300 duration-500 line-through text-left"
-                                      : "capitalize text-gray-600 duration-500 text-left"
-                                  }
-                                >
-                                  {task.task}
-                                </p>
-                                <p
-                                  className={
-                                    task.complete
-                                      ? "capitalize text-black opacity-100 duration-500 font-semibold"
-                                      : "duration-500 opacity-0 capitalize font-semibold"
-                                  }
-                                >
-                                  Completed
-                                </p>
-                              </div>
-                              <img
+        <div className="w-2/6 m-5 text-gray-600">
+          <DragDropContext onDragEnd={handleOnDragEnd} onDragStart={handleDrag}>
+            <Droppable droppableId="taks">
+              {(provided) => (
+                <section {...provided.droppableProps} ref={provided.innerRef} className={isDragged ? 'w-full opacity-50 duration-500' : 'w-full duration-500'}>
+                  {todoList.map((task, index) => {
+                    return (
+                      <Draggable
+                        key={task.id}
+                        draggableId={task.id.toString()}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <section
+                            className="w-full flex justify-center"
+                            {...provided.draggableProps}
+                            ref={provided.innerRef}
+                          >
+                            <img
+                              src={drag}
+                              alt=""
+                              {...provided.dragHandleProps}
+                              onDrag={handleDrag}
+                              className="w-5 m-2 duration-500"
+                              style={{display: task.deleted ? 'none' : 'block'}}
+                            />
+                            <div
+                              className={
+                                task.deleted
+                                  ? "hidden"
+                                  : "flex justify-between items-center border-2 border-solid border-black px-5 py-2 my-2 w-full cursor-pointer "
+                              }
+                              complete={String(task.complete)}
+                              onClick={() => handleCompleteTask(task.id)}
+                            >
+                              <p
                                 className={
-                                  task.trashNear ? "animate__infinite animate__heartBeat cursor-pointer duration-500 w-10" : "animate-none cursor-pointer duration-500 w-10"
+                                  task.complete
+                                    ? "capitalize text-gray-300 duration-500 line-through text-left text-ellipsis"
+                                    : "capitalize text-gray-600 duration-500 text-left text-ellipsis"
                                 }
-                                style= {
-                                  {'display': task.deleted ? 'none' : 'block'}
+                              >
+                                {task.task}
+                              </p>
+                              <p
+                                className={
+                                  task.complete
+                                    ? "capitalize text-black opacity-100 duration-500 font-semibold"
+                                    : "duration-500 opacity-0 capitalize font-semibold"
                                 }
-                                src={trashCan}
-                                alt="trashCan"
-                                onMouseEnter={() => handleDistance(task.id)}
-                                onMouseLeave={() => handleDistance(task.id)}
-                                onClick={() => handleDelete(task.id)}
-                              />
-                            </section>
-                          )}
-                        </Draggable>
-                      );
-                    })}
-                    {provided.placeholder}
+                              >
+                                Completed
+                              </p>
+                            </div>
+                            <img
+                              className={
+                                task.trashNear
+                                  ? "animate__infinite animate__heartBeat cursor-pointer duration-500 w-10"
+                                  : "animate-none cursor-pointer duration-500 w-10"
+                              }
+                              style={{
+                                display: task.deleted ? "none" : "block",
+                              }}
+                              src={trashCan}
+                              alt="trashCan"
+                              onMouseEnter={() => handleDistance(task.id)}
+                              onMouseLeave={() => handleDistance(task.id)}
+                              onClick={() => handleDelete(task.id)}
+                            />
+                          </section>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
                 </section>
-                )}
-              </Droppable>
-            </DragDropContext>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
       </div>
     </div>
   );
 }
+
 export default TODO_LIST;
